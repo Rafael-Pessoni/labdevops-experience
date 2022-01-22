a6702f9f0ba0533277fc8ea93a681a055d6a976f
Rafael Pessoni <Rafael.pessoni@outlook.com>
2022-01-22 15:43:16 -0300

    Teste Fail Pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Rafael Pessoni"
+    return "Hello World - Rafael Pessoni - FIAP"
 
 if __name__ == '__main__':
     app.run()
